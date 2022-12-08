ac3e073a63deb0624dcb358dce729d93c0b6ec4b
JaiiBarrier <109163810+JaiiBarrier@users.noreply.github.com>
2022-12-08 12:09:24 -0600

    exported from Skill Struck

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+print("SPOOKY STORIES")
+print("BY: Count Skill Spooky")
+
+adj1 = input("First Adjective")
+first_name = input("First Name")
+adj2 = input("Second Adjective")
+noun1 = input("Noun")
+verb = input("Verb")
+verb_ing = input('Verb ending in "ing" ')
+president = input("Name any president of the US")
+
+print("They all say my school is haunted! My " + adj1 + " talking rabbit " + first_name + " says he/she saw a " + adj2 + " " + noun1 + " floating at the end of the hall near the cafeteria.  Some say if you " + verb + " down that hallway at night, you'll hear a wild " + verb_ing + " " + president + " that looks so real!!")
+
+print("Signed: Scary Pants")
