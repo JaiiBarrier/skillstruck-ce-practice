caae19cf6f16a734a33e9746638cd0b0883834e2
JaiiBarrier <109163810+JaiiBarrier@users.noreply.github.com>
2022-12-08 12:09:24 -0600

    exported from Skill Struck

---
+++ PythonTeaching.py
@@ -0,0 +1,216 @@
+#	THIS IS A COMMENT THAT WILL NOT AFFECT THE CODE. 
+
+
+#BASIC PYTHON RULES: 
+#	Line Order - from Top to Bottom 
+
+#	Whitespace - variable1 vs variable 1
+
+#	Case Sensitive
+# 		print("Hello World") vs Print("Hello World")
+ 
+	
+	
+	
+	
+	
+	
+#WHAT IS A PRINT STATEMENT?
+#  The output of what the user & console sees
+
+
+# -- TYPE & TRY THIS EXAMPLE -- print("Hello World")
+# print("Hey Yalllllll")
+
+# print("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# WHAT IS A VARIABLE? - An Identifier.   A building block for programming that is basically a holding box for different data types. Think of your name!
+
+
+# 1. Must either be one word, or connected with underscores.  
+
+
+# VALID
+# name = "Jaii"
+
+# first_name ="Jaii"
+
+# INVALID
+
+# first_name = "Jaii"
+
+
+
+
+
+# 2. A variable name must start with a letter or an underscore character _.  It CANNOT start with a number. 
+# AND No special characters are allowed.
+
+
+# VALID
+
+
+# INVALID
+
+
+
+
+# How do we print a variable? Line Order
+
+
+
+# What's the difference between print("car") & print(car)?
+
+
+# car = "Benz"
+# # print("car")
+
+# print(car)
+
+
+
+
+
+
+
+
+# *********** ANY QUESTIONS????? *********** 
+
+
+
+#	Can you combine variables together? 
+#		Yes - with Concatenation! But How? 
+
+
+
+# car = "red"
+# new_car = "blue"
+
+
+
+
+# print("Words right here" + " " + car)
+
+
+
+
+
+# *********** ANY QUESTIONS????? *********** 
+
+
+
+
+
+
+
+
+
+
+#What are the different Variable Types?
+# 	String // name = "age"
+# 	Integer // age = 22
+# 	Boolean // hungry = True
+
+
+
+
+# STRING - a text value
+
+
+
+
+
+
+
+# INTEGER / FLOAT - two different ways to display numbers
+
+
+
+
+
+
+# How do I execute math in Python?
+# ADDITION [+]
+
+
+age = 5
+new_year = 7
+
+
+
+
+# SUBTRACTION [-]
+
+
+
+
+# MULTIPLICATION [*]
+
+
+
+
+
+# DIVISION [/]
+
+
+
+
+
+
+
+#What is Input/Output?
+#	Inviting the user to give a response (input) for the program to process and display (output).
+
+
+
+
+
+first_name = input("What is your first name?")
+
+print(first_name)
+
+
+print("My name is " + first_name)
+
+
+
+
+
+
+
+#Let's Create our MadLib!
+
+
+
+
+
+
+
