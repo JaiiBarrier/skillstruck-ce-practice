6f596787481516e0b5e546dcf518c2867639b81e
JaiiBarrier <109163810+JaiiBarrier@users.noreply.github.com>
2022-12-08 12:09:25 -0600

    exported from Skill Struck

---
+++ CodeEvent.py
@@ -0,0 +1,7 @@
+print("code Spooky Stories!!!!")
+
+Adj = input( " Place an Adjective" )
+animal = input( " Place an Animal" )
+Number = input( " Place a Number" )
+
+print("This year for Halloween I will dress up as a/an " + Adj + " " + animal + "! Please have the $" + Number + " prize ready!")
